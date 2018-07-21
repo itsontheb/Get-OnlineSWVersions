6de8342287782b34c07ccb8608069b010c141099
itsontheb <itsontheb@users.noreply.github.com>
2018-07-21 16:42:35 -0400

    Add Function: Get-OnlineVerTEMPLATE
    Just a template for the Get-OnlineVer* functions

---
+++ WorkingScripts/Get-OnlineVerTEMPLATE.ps1
@@ -0,0 +1,47 @@
+﻿Get-OnlineVerTEMPLATE
+{
+    [cmdletbinding()]
+    param (
+        [switch]$Quiet
+    )
+
+    # Variables
+    $URI = 'TEMPLATE'
+    $SoftwareName = 'TEMPLATE'
+
+    $hashtable = [ordered]@{
+        'Software_Name'    = $softwareName
+        'Software_URL'     = $uri
+        'Online_Version'   = 'UNKNOWN' 
+        'Online_Date'      = 'UNKNOWN'
+        'Download_URL_x86' = 'UNKNOWN'
+        'Download_URL_x64' = 'UNKNOWN'
+    }
+    
+    $swObject = New-Object -TypeName PSObject -Property $hashtable
+
+    try
+    {
+
+    }
+    catch
+    {
+        $message = ("Line {0} : {1}" -f $_.InvocationInfo.ScriptLineNumber, $_.exception.message)
+        Write-Warning $message
+    }
+    finally
+    {
+        
+    }
+
+    # Output to Host
+    if ($Quiet)
+    {
+        Return $swObject.Online_Version
+    }
+    else
+    {
+        Return $swobject
+    }
+
+} # END Function Get-OnlineVerTEMPLATE
