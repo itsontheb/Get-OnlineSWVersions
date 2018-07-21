ec4f564f1597ec5649ecb5f5bdbc741e330dbcd1
itsontheb <itsontheb@users.noreply.github.com>
2018-07-21 16:52:08 -0400

    Cleanup
    Cleaned up params and comments

--- WorkingScripts/Get-OnlineVerTEMPLATE.ps1
+++ WorkingScripts/Get-OnlineVerTEMPLATE.ps1
@@ -1,13 +1,42 @@
-﻿Get-OnlineVerTEMPLATE
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+.INPUTS
+   Inputs to this cmdlet (if any)
+.OUTPUTS
+   Output from this cmdlet (if any)
+.NOTES
+   General notes
+.COMPONENT
+   The component this cmdlet belongs to
+.ROLE
+   The role this cmdlet belongs to
+.FUNCTIONALITY
+   The functionality that best describes this cmdlet
+#>
+
+Get-OnlineVerTEMPLATE
 {
     [cmdletbinding()]
     param (
+        [Parameter(Mandatory=$false, 
+                   Position=0)]
+        [Alias("SW")]
+        $SoftwareName = 'TEMPLATE',
+        [Parameter(Mandatory=$false, 
+                   Position=1)]
+        [Alias("URL")]
+        [string]$URI = 'TEMPLATE',
+        [Parameter(Mandatory=$false, 
+                   Position=2)]
         [switch]$Quiet
     )
-
-    # Variables
-    $URI = 'TEMPLATE'
-    $SoftwareName = 'TEMPLATE'
 
     $hashtable = [ordered]@{
         'Software_Name'    = $softwareName
